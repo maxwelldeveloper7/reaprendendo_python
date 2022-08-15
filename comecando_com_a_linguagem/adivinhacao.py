53cb78b752b8e306382eb979e23e30dbbb49aff9
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-08-15 14:57:41 -0300

    comparando variáveis

--- comecando_com_a_linguagem/adivinhacao.py
+++ comecando_com_a_linguagem/adivinhacao.py
@@ -1,1 +1,16 @@
+print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = int(input("Digite o seu número: "))
+
+print("Você digitou ", chute)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")#02. comparando variáveis
+
+print("Fim do jogo")
