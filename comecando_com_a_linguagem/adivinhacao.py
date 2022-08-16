6647b05f18861bca82528312b36854a7373ace86
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-08-15 21:11:24 -0300

    formatação strings

--- comecando_com_a_linguagem/adivinhacao.py
+++ comecando_com_a_linguagem/adivinhacao.py
@@ -3,14 +3,30 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
 
-print("Você digitou ", chute)
+while(rodada <= total_de_tentativas):
 
-if(numero_secreto == chute):
-    print("Você acertou!")
-else:
-    print("Você errou!")#02. comparando variáveis
+    print("Tentativa {} de {}", format(rodada, total_de_tentativas))
+
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou ", chute)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou!")
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto")
+
+    
+    rodada += 1
 
 print("Fim do jogo")
