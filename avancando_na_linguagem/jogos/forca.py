65a15dc777707d4c0ab74cfef9378ba9da5a3ee3
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-09-12 09:49:28 -0300

    continuar da aula 6

--- avancando_na_linguagem/jogos/forca.py
+++ avancando_na_linguagem/jogos/forca.py
@@ -6,21 +6,41 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    '''
+        for letra in palavra_secreta:
+            letras_acertadas.append("_")
+    '''
     enforcou = False
     acertou = False
 
+    erros = 0
+
+    print(letras_acertadas)
+
     while(not enforcou and not acertou):
-        chute = input("Qual letra?:").strip()
+        chute = input("Qual letra?:").strip().upper()
 
-        index = 0
+        if(chute in palavra_secreta):            
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    #print("Encontrei a letra {} na posição {}".format(letra, index))
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
 
+        print(letras_acertadas)
+
+        if(acertou):
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
 
     print("Fim do jogo")
 
