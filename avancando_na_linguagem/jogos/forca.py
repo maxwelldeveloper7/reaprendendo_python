72e24ba48a272f89593197314d30b0751bd7879c
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-09-13 15:50:06 -0300

    130920221550

--- avancando_na_linguagem/jogos/forca.py
+++ avancando_na_linguagem/jogos/forca.py
@@ -1,12 +1,95 @@
-from xml.etree.ElementInclude import include
+import random
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("./jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def jogar():
+    
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?:")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):            
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    #print("Encontrei a letra {} na posição {}".format(letra, index))
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+
+        if(acertou):
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
 
 
+"""
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+
+    arquivo = open("./jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
+    print(palavras)
+
+    
     letras_acertadas = ["_" for letra in palavra_secreta]
     '''
         for letra in palavra_secreta:
@@ -45,4 +128,5 @@ def jogar():
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
-    jogar()#continuar de lendo um arquivo
+    jogar()
+"""
