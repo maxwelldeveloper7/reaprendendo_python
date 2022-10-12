eb51d4b9e0bdbb4fa5e15f1e1da2c330ac441974
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-11 23:25:15 -0300

    aula3

--- avancando_oo/modelo.py
+++ avancando_oo/modelo.py
@@ -18,24 +18,35 @@ class Programa:
     @nome.setter
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
+    
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} minutos - {self._likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+    
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_likes()
-print(f'{vingadores.nome} -  {vingadores.duracao}: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'{atlanta.nome} - {atlanta.ano}: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
