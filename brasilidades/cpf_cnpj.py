b456462c43559d4b3d51fd47942a8f3d5877f50a
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-13 17:33:50 -0300

    novos métodos

--- brasilidades/cpf_cnpj.py
+++ brasilidades/cpf_cnpj.py
@@ -3,12 +3,21 @@ from validate_docbr import CNPJ
 
 class CpfCnpj:
     
-    def __init__(self, documento):
+    def __init__(self, documento, tipo_documento):
+        self.tipo_documento = tipo_documento
         documento = str(documento)
-        if self.cpf_e_valido(documento):
-            self.cpf = documento
+        if self.tipo_documento == "cpf":
+            if self.cpf_e_valido(documento):
+                self.cpf = documento
+            else:
+                raise ValueError("CPF inválido!!")
+        elif self.tipo_documento == "cnpj":
+            if self.cnpj_e_valido(documento):
+                self.cnpj = documento
+            else:
+                raise ValueError("CNPJ inválido!!")
         else:
-            raise ValueError("CPF inválido!!")        
+            raise ValueError("Documento inválido")
     
     def cpf_e_valido(self, cpf):
         if len(cpf) == 11:
@@ -21,12 +30,19 @@ class CpfCnpj:
         mascara = CPF()
         return mascara.mask(self.cpf)
 
+    def formata_cnpj(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
+
     def __str__(self):
-        return self.formata_cpf()
+        if self.tipo_documento == "cpf":
+            return self.formata_cpf()
+        elif self.tipo_documento == "cnpj":
+            return self.formata_cnpj()
     
     def cnpj_e_valido(self, cnpj):
         if len(cnpj) == 14:
             validador = CNPJ()
             return validador.validate(cnpj)
         else:
-            raise ValueError("CNPJ inválido!!")
+            raise ValueError("Quantidade de dígitos inválida!!")
