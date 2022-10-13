462049a8fe01d743419a6e3b6baad7dbb61e5531
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-13 17:34:05 -0300

    testando

--- brasilidades/main.py
+++ brasilidades/main.py
@@ -1,7 +1,10 @@
 from cpf_cnpj import CpfCnpj
 
 cpf = '04960780622'
+cnpj = '28975623000107'
 
-objeto_cpf = CpfCnpj(cpf)
+objeto_cpf = CpfCnpj(cpf,'cpf')
+objeto_cnpj = CpfCnpj(cnpj,'cnpj')
 
 print(objeto_cpf)
+print(objeto_cnpj)
