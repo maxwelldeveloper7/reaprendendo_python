bc4a285a162ae0e97e459fa8bfb999d9d0ba7a23
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-12 23:03:08 -0300

    implementando validade_docbr

--- brasilidades/cpf.py
+++ brasilidades/cpf.py
@@ -1,3 +1,5 @@
+from validate_docbr import CPF
+
 class Cpf:
     
     def __init__(self, documento):
@@ -10,16 +12,13 @@ class Cpf:
     def __str__(self):
         return self.formata_cpf()
     
-    def cpf_e_valido(self, documento):
-        if len(documento) == 11:
-            return True
+    def cpf_e_valido(self, cpf):
+        if len(cpf) == 11:
+            validador = CPF()
+            return validador.validate(cpf)
         else:
-            return False
+            raise ValueError("Quantidade de dígitos inválida!!")
         
     def formata_cpf(self):
-        fatia_um = self.cpf[:3]
-        fatia_dois = self.cpf[3:6]
-        fatia_tres = self.cpf[6:9]
-        fatia_quatro = self.cpf[9:]
-
-        return f'{fatia_um}.{fatia_dois}.{fatia_tres}-{fatia_quatro}'
+        mascara = CPF()
+        return mascara.mask(self.cpf)
