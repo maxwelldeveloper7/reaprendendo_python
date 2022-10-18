4e79998fbd001f676a8789a362ae07c3684df782
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-17 22:40:58 -0300

    testando implementações

--- brasilidades/main.py
+++ brasilidades/main.py
@@ -1,4 +1,11 @@
-from cpf_cnpj import Documento
+import re
+from telefones import Telefones
+
+telefone = "33988117010"
+
+telefone_obj = Telefones(telefone)
+
+'''from cpf_cnpj import Documento
 
 cpf = '04960780622'
 cnpj = '28975623000107'
@@ -8,3 +15,4 @@ objeto_cnpj = Documento.cria_documento(cnpj)
 
 print(objeto_cpf)
 print(objeto_cnpj)
+'''
