441369008b10b0e8d200c868fbefd9b4c6f03301
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-20 17:04:28 -0300

    testando

---
+++ pep8/main.py
@@ -0,0 +1,9 @@
+from fila_normal import FilaNormal
+
+fila_teste = FilaNormal()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(fila_teste.chama_cliente(5))
+print(fila_teste.chama_cliente(10))
