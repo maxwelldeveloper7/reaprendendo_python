3fbec8aae3bc2ef376bdd6cdf4b24ad574fd2458
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-20 17:04:49 -0300

    construindo nossa classe

--- pep8/fila_normal.py
+++ pep8/fila_normal.py
@@ -6,3 +6,19 @@ class FilaNormal:
     
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'NM{self.codigo}'
+        
+    def reseta_vila(self) -> None:
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
+    
+    def atualiza_fila(self) -> None:
+        self.reseta_vila()
+        self.gera_senha_atual()
+        self.fila.append(self.senha_atual)
+        
+    def chama_cliente(self, caixa:int) -> str:
+        cliente_atual:str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return(f'Cliente atual: {cliente_atual}, digija-se ao caixa: {caixa}')
