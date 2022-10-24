5d39b869f7fcef4691236a9ff47ee064083cc1ef
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-24 15:47:53 -0300

    classe abstrata

--- pep8/fila_base.py
+++ pep8/fila_base.py
@@ -1,4 +1,7 @@
-class FilaBase:
+import abc
+
+
+class FilaBase(metaclass=abc.ABCMeta):
     codigo: int = 0
     fila = []
     clientes_atendidos = []
@@ -9,3 +12,18 @@ class FilaBase:
             self.codigo = 0
         else:
             self.codigo += 1
+
+
+@abc.abstractmethod
+def gera_senha_atual(self):
+    ...
+
+
+@abc.abstractmethod
+def atualiza_fila(self):
+    ...
+
+
+@abc.abstractmethod
+def chama_cliente(self, caixa: int):
+    ...
