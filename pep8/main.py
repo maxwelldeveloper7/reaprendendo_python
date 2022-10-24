1b680c4565c96041cc7fb63c4a26ae7e4709953a
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-24 20:41:09 -0300

    testando

--- pep8/main.py
+++ pep8/main.py
@@ -1,19 +1,26 @@
-from fila_normal import FilaNormal
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
-from fila_prioritaria import FilaPrioritaria
 
-fila_teste = FilaNormal()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-print(fila_teste.chama_cliente(5))
-print(fila_teste.chama_cliente(10))
+# fila_teste = FilaNormal()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# print(fila_teste.chama_cliente(5))
+# print(fila_teste.chama_cliente(10))
 
-fila_teste_2 = FilaPrioritaria()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-print(fila_teste_2.chama_cliente(10))
-print(fila_teste_2.chama_cliente(1))
-print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+# fila_teste_2 = FilaPrioritaria()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+# print(fila_teste_2.chama_cliente(1))
+# print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila(('prioritaria'))
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
