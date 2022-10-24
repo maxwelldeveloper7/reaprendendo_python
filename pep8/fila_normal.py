1e063f929e520aa993c010804fde33cf13eaa5a3
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-24 12:47:09 -0300

    importatando fila base e adequando ao pep8

--- pep8/fila_normal.py
+++ pep8/fila_normal.py
@@ -1,24 +1,16 @@
-class FilaNormal:
-    codigo: int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ""
-    
-    def gera_senha_atual(self)-> None:
+from fila_base import FilaBase
+
+
+class FilaNormal(FilaBase):
+    def gera_senha_atual(self) -> None:
         self.senha_atual = f'NM{self.codigo}'
-        
-    def reseta_vila(self)-> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
-    
-    def atualiza_fila(self)-> None:
+
+    def atualiza_fila(self) -> None:
         self.reseta_vila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
-        
-    def chama_cliente(self, caixa: int)-> str:
+
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
-        return(f'Cliente atual: {cliente_atual}, digija-se ao caixa: {caixa}')
+        return (f'Cliente atual: {cliente_atual}, digija-se ao caixa: {caixa}')
