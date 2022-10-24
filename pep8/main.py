186e0e63e2e0590b195aca5a1138cf97252dc2a0
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-24 15:47:27 -0300

    testando

--- pep8/main.py
+++ pep8/main.py
@@ -1,13 +1,13 @@
-# from fila_normal import FilaNormal
+from fila_normal import FilaNormal
 from fila_prioritaria import FilaPrioritaria
 
-# fila_teste = FilaNormal()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# print(fila_teste.chama_cliente(5))
-# print(fila_teste.chama_cliente(10))
+fila_teste = FilaNormal()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(fila_teste.chama_cliente(5))
+print(fila_teste.chama_cliente(10))
 
 fila_teste_2 = FilaPrioritaria()
 fila_teste_2.atualiza_fila()
