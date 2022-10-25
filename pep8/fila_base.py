9b73de1b92280a3677a2ae150823f1f91469af7a
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-10-24 21:07:46 -0300

    type hints

--- pep8/fila_base.py
+++ pep8/fila_base.py
@@ -1,14 +1,13 @@
 import abc
+from typing import List
 
-from constantes import TAMANHO_PADRAO_MAXIMO
-from constantes import TAMANHO_PADRAO_MINIMO
+from constantes import TAMANHO_PADRAO_MAXIMO, TAMANHO_PADRAO_MINIMO
 
 
 class FilaBase(metaclass=abc.ABCMeta):
-
     codigo: int = 0
-    fila = []
-    clientes_atendidos = []
+    fila: List[str] = []
+    clientes_atendidos: List[str] = []
     senha_atual: str = ""
 
     def reseta_fila(self) -> None:
