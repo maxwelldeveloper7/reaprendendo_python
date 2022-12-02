579a63cb4df3d9c439a6439ebaa9d9c043817a1e
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-12-02 11:39:53 -0300

    try e except

--- tratamento_erros/example.py
+++ tratamento_erros/example.py
@@ -1,8 +1,18 @@
-def dividir(dividendo, divisor):
+def dividir(dividendo: int, divisor: int) -> float:
+    # print(divisor.resultado)
     return dividendo / divisor
 
-def testa_divisao(divisor):
-    resultado = dividir(10, divisor)
-    print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
 
-testa_divisao(0)
+def testa_divisao(divisor: int) -> str:
+    try:
+        resultado: float = dividir(10, divisor)
+        print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
+    except ZeroDivisionError:
+        print('Erro de divisão por zero tratado')
+
+
+try:
+    testa_divisao(0)
+except AttributeError:
+    print('Erro de atributo tratado')
+print('Programa encerrado')
