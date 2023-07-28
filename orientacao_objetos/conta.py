0b15ba48415d1bdfe4ca1b049259597d6094fd48
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-07-28 11:32:52 -0300

    aula 04

--- orientacao_objetos/conta.py
+++ orientacao_objetos/conta.py
@@ -1,16 +1,21 @@
 
 class Conta:
     def __init__(self, numero, titular, saldo, limite) -> None:
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo de {self.saldo} do titular {self.titular}.")
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}.")
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
