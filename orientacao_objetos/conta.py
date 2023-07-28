0a4f78e30daaa456a2fe8f233871d4855482b493
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2023-07-28 16:48:19 -0300

    fim do curso

--- orientacao_objetos/conta.py
+++ orientacao_objetos/conta.py
@@ -10,8 +10,15 @@ class Conta:
     def extrato(self):
         print(f"Saldo de {self.__saldo} do titular {self.__titular}.")
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def deposita(self, valor):
-        self.__saldo += valor
+        if self.__pode_sacar(valor):
+            self.__saldo += valor
+        else:
+            print("O valor {} passsou o limite".format(valor))
 
     def saca(self, valor):
         self.__saldo -= valor
@@ -35,3 +42,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
